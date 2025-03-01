f309adbdef21fdb28c712e9e262e1d99cd95f1c0
gkerherv <g.kerherve@ic.ac.uk>
2025-03-01 17:11:37 +0000

    Mac: Test GitHub 3

--- libraries/Help.py
+++ libraries/Help.py
@@ -469,7 +469,7 @@ def show_version_log(window):
     version_log = """Version 1.5 (April 2025)
 - Added File > New File in the menu
 - Added File > Save as... in the menu
-- Added File > Test 2
+- Added File > Test 3
 Version 1.4 (February 2025)
 - Mac version coming soon
 - Added Skewed Voigt model. Default to leastsq fitting method
